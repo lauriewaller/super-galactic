269316e2866a87fed7ba6238cadf045c197cd96c
Laurie Waller <lauriewaller7@gmail.com>
2021-03-13 12:06:58 -0800

    create user class and constructor

---
+++ src/js/user.js
@@ -0,0 +1,11 @@
+export default class User {
+  
+  constructor(earthAge, lifeExpectancy) {
+    this.earthAge = earthAge;
+    this.lifeExpectancy = lifeExpectancy;
+    this.mercuryAge = earthAge * .24;
+    this.venusAge = earthAge * .62;
+    this.marsAge = earthAge * 1.88;
+    this.jupiterAge = earthAge * 11.86;
+  }
+};
