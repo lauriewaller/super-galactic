d1018d0adb426acf88119a62d476fc19d63ed3a9
Laurie Waller <lauriewaller7@gmail.com>
2021-03-13 11:58:04 -0800

    write test for User constructor and earthAge variable

---
+++ __tests__/user.test.js
@@ -0,0 +1,15 @@
+import User from './../src/js/user.js';
+
+describe ('User', () => {
+
+  let currentUser;
+
+  beforeEach(() => {
+    currentUser = new User(37, 77);
+  });
+
+  test('should correctly create an object with earthAge property using the Object constructor', () => {
+      expect(currentUser.earthAge).toEqual(37);
+    });
+
+});
