1544a3085072e732487822379afe294db3a2dac9
Laurie Waller <lauriewaller7@gmail.com>
2021-03-13 13:11:07 -0800

    create good fail for mercyuryAge property test

--- src/js/user.js
+++ src/js/user.js
@@ -2,7 +2,7 @@ export default class User {
   
   constructor(earthAge, lifeExpectancy) {
     this.earthAge = earthAge;
-    this.mercuryAge = earthAge * .24;
+    this.mercuryAge = earthAge * 2;
     this.venusAge = earthAge * .62;
     this.marsAge = earthAge * 1.88;
     this.jupiterAge = earthAge * 11.86;
