a7525c3395a886012ade6fe7485be4c4aaf2fd37
Laurie Waller <lauriewaller7@gmail.com>
2021-03-13 19:29:52 -0800

    correct file tree

--- src/js/contact.js
+++
@@ -1,9 +0,0 @@
-export default function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-}
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-};
